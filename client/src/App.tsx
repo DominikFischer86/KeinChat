765a7dd7578c5769c5d7c915e4c104a1fdb2b812
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-11-19 01:55:52 +0100

    feat: Ad Sidebar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
 import Login from './components/Login'
 import { useLocalstorage } from './hooks/useLocalstorage'
 
+import Dashboard from './components/Dashboard'
+
 import './App.scss'
 
 const App = () => {
-    const [id, setId] = useLocalstorage("id", null)
+    const [id, setId] = useLocalstorage('id', null)
 
-    return (
-        <>
-            {id}
-            <Login onIdSubmit={setId} />
-        </>
-    )
+    return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
 }
 
 export default App
---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+type SidebarProps = {
+    id: string
+}
+
+const Sidebar = ({ id }: SidebarProps) => {
+    return <div>Sidebar of {id}</div>
+}
+
+export default Sidebar
