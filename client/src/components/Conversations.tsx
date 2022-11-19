167f6e8e170c524b72b6a55327121eb2c3cf6c8e
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-11-19 22:26:21 +0100

    feat: Add Conversationslist and Modal

--- client/src/components/Conversations.tsx
+++ client/src/components/Conversations.tsx
@@ -1,5 +1,47 @@
+import { useConversations } from "../context/ConversationsProvider"
+
+const MAX_RECIPIENTS_BEFORE_TRIM = 2
+const MAX_NAME_LENGTH_BEFORE_TRIM = 14
+
+const trimLongAssNames = (name: string) => {
+  if (name.length > MAX_NAME_LENGTH_BEFORE_TRIM)
+    return name.substring(0, MAX_NAME_LENGTH_BEFORE_TRIM) + "..."
+  return name
+}
+
+const recipientList = (conversation: any, recipient: any, index: number) => {
+  const activeRecipients = conversation.recipients.length
+
+  if (activeRecipients > MAX_RECIPIENTS_BEFORE_TRIM)
+    return index === 0
+      ? `${recipient.name} and ${
+          activeRecipients - MAX_RECIPIENTS_BEFORE_TRIM + 1
+        } others`
+      : null
+  return index === activeRecipients - 2
+    ? trimLongAssNames(recipient.name) + ", "
+    : trimLongAssNames(recipient.name)
+}
+
 const Conversations = () => {
-  return <>Conversations</>
+  const { conversations } = useConversations()
+
+  return (
+    <ul className="list-group">
+      {conversations.map((conversation: any, index: number) => (
+        <li
+          key={index}
+          title={conversation.recipients.map(
+            (recipient: any) => " " + recipient.name
+          )}
+        >
+          {conversation.recipients.map((recipient: any, index: number) =>
+            recipientList(conversation, recipient, index)
+          )}
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default Conversations
--- client/src/components/NewConversationModal.tsx
+++ client/src/components/NewConversationModal.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react"
+
+import { useContacts } from "../context/ContactsProvider"
+import { useConversations } from "../context/ConversationsProvider"
+
 type NewConversationModalProps = {
   title: string
   closeModal: () => void
@@ -7,13 +12,51 @@ const NewConversationModal = ({
   title,
   closeModal,
 }: NewConversationModalProps) => {
+  const [selectedContactIds, setSelectedContactIds]: any = useState([])
+
+  const { contacts } = useContacts()
+  const { createConversation } = useConversations()
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    createConversation(selectedContactIds)
+    closeModal()
+  }
+
+  const handleCheckboxChange = (id: string) => {
+    setSelectedContactIds((prevIds: string[]) => {
+      if (prevIds.includes(id))
+        return prevIds.filter((prevId: string) => id !== prevId)
+      return [...prevIds, id]
+    })
+  }
+
   return (
     <>
       <div className="modal-header">
         <h2>{title}</h2>
         <button onClick={closeModal}>X</button>
       </div>
-      <div className="modal-content">Body</div>
+      <div className="modal-content">
+        <form onSubmit={handleSubmit}>
+          {contacts.map((contact: any) => (
+            <div key={contact.id} className="form-checkbox-group">
+              <input
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                onChange={() => handleCheckboxChange(contact.id)}
+              />
+              <label>{contact.name}</label>
+            </div>
+          ))}
+          <button
+            disabled={!selectedContactIds.length}
+            className="submit-button"
+          >
+            Create
+          </button>
+        </form>
+      </div>
     </>
   )
 }
