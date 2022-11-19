72bee350eb230e7fa955aea1e64aec6fd9c5f504
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-11-19 14:43:34 +0100

    feat: Add modal base for Conversations & contacts

---
+++ client/src/components/NewContactModal.tsx
@@ -0,0 +1,9 @@
+type NewContactModalProps = {
+  id: string
+}
+
+const NewContactModal = () => {
+  return <div className="NewContactModal-container">Contact Modal</div>
+}
+
+export default NewContactModal
---
+++ client/src/components/NewConversationModal.tsx
@@ -0,0 +1,11 @@
+type NewConversationModalProps = {
+  id: string
+}
+
+const NewConversationModal = () => {
+  return (
+    <div className="NewConversationModal-container">Conversation Modal</div>
+  )
+}
+
+export default NewConversationModal
