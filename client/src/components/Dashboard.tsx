de0801335aafcc4fa277ab73982f5411534e48e5
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-11-19 14:43:47 +0100

    style: Linting

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,11 +1,17 @@
 import Sidebar from "./Sidebar"
 
+import "./Dashboard.scss"
+
 type DashboardProps = {
   id: string
 }
 
 const Dashboard = ({ id }: DashboardProps) => {
-  return <Sidebar id={id} />
+  return (
+    <div className="dashboard-container">
+      <Sidebar id={id} />
+    </div>
+  )
 }
 
 export default Dashboard
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,9 +1,65 @@
+import { useState } from "react"
+
+import Conversations from "./Conversations"
+import Contacts from "./Contacts"
+import Modal from "./Modal"
+import NewContactModal from "./NewContactModal"
+import NewConversationModal from "./NewConversationModal"
+
+import "./Sidebar.scss"
+
 type SidebarProps = {
-    id: string
+  id: string
 }
 
+const CONVERSATIONS_KEY = "conversations"
+const CONTACT_KEY = "contacts"
+
 const Sidebar = ({ id }: SidebarProps) => {
-    return <div>Sidebar of {id}</div>
+  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+  const conversationOpen = activeKey === CONVERSATIONS_KEY
+
+  return (
+    <div className="sidebar-container">
+      <nav className="tab-container">
+        <ul>
+          <li className={activeKey === CONVERSATIONS_KEY ? "active" : ""}>
+            <button onClick={() => setActiveKey(CONVERSATIONS_KEY)}>
+              Conversations
+            </button>
+          </li>
+          <li className={activeKey === CONTACT_KEY ? "active" : ""}>
+            <button onClick={() => setActiveKey(CONTACT_KEY)}>Contacts</button>
+          </li>
+        </ul>
+      </nav>
+      <div className="tab-content">
+        {activeKey === CONVERSATIONS_KEY && <Conversations />}
+        {activeKey === CONTACT_KEY && <Contacts />}
+      </div>
+      <div className="id-field">
+        Your Id: <p>{id}</p>
+      </div>
+      <button
+        onClick={() => setModalOpen(true)}
+        className="sidebar-create-button"
+      >
+        New {conversationOpen ? "Conversation" : "Contact"}
+      </button>
+      <Modal
+        title={`New ${conversationOpen ? "Conversation" : "Contact"}`}
+        show={modalOpen}
+        onHide={closeModal}
+      >
+        {conversationOpen ? <NewConversationModal /> : <NewContactModal />}
+      </Modal>
+    </div>
+  )
 }
 
 export default Sidebar
