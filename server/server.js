ad59491deda5886e1491f4f1f377ce3501742e53
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-11-22 00:08:21 +0100

    build: Install and setup socket.io

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import { Server } from "socket.io"
+
+const io = new Server(5000, {
+  cors: {
+    origin: ["http://127.0.0.1:5173"]
+  }
+})
+
+io.on("connection", socket => {
+  const id = socket.handshake.query.id
+  socket.join(id)
+
+  socket.on("send-message", ({ recipients, text }) => {
+     recipients?.forEach((recipient) => {
+       const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+       socket.broadcast.to(recipient).emit("receive-message", {
+         recipients: newRecipients, sender: id, text
+      })
+    })
+   })
+})
